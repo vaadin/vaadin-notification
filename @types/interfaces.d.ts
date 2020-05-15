1ecda0fa93b82885194cb21c7a5486675d921c9d
Kari Söderholm <haprog@vaadin.com>
2020-05-15 13:09:56 +0300

    fix: minor update to TS defs PR

--- @types/interfaces.d.ts
+++ @types/interfaces.d.ts
@@ -1,10 +1,10 @@
-import { NotificationCard, NotificationElement } from '../src/vaadin-notification.js';
+import { NotificationElement } from '../src/vaadin-notification.js';
 
 export type NotificationPosition =
   'top-stretch' | 'top-start' | 'top-center' | 'top-end' | 'middle' | 'bottom-start' | 'bottom-center' | 'bottom-end' |
   'bottom-stretch';
 
 export type NotificationRenderer = (
-  root: NotificationCard,
+  root: HTMLElement,
   notification: NotificationElement
 ) => void;
